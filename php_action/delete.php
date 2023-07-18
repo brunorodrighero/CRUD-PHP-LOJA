f2391373c761d2d70655fc8fe2b2d92a322324a9
Bruno Luiz Silva Rodighiero <66458327+brunorodrighero@users.noreply.github.com>
2023-07-17 20:24:33 -0400

    Update delete.php -  aumentam a segurança e a eficiência do seu código PHP, mantendo a mesma funcionalidade que o código original.

--- php_action/delete.php
+++ php_action/delete.php
@@ -1,21 +1,28 @@
 <?php
-    //sessão
     session_start();
-    //Conexão
+
     require_once "db_connect.php";
 
-    if(isset($_POST['btn-deletar'])):
-        $id=mysqli_escape_string($connect, $_POST['id']);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $id = $_POST['id'] ?? '';
 
-        $sql="DELETE FROM estoque WHERE id='$id'";
+        $sql = "DELETE FROM estoque WHERE id = ?";
 
-        if(mysqli_query($connect, $sql)):
-            header("Location: ../consultar.php?sucesso");
-        else:
-            header("Location: '../consultar.php?erro");
-        
-    endif;
-    
-endif;
+        if ($stmt = mysqli_prepare($connect, $sql)) {
+            mysqli_stmt_bind_param($stmt, "i", $id);
 
+            if (mysqli_stmt_execute($stmt)) {
+                header("Location: ../consultar.php?sucesso", true, 303);
+                exit;
+            } else {
+                header("Location: ../consultar.php?erro", true, 303);
+                exit;
+            }
+
+            mysqli_stmt_close($stmt);
+        } else {
+            header("Location: ../consultar.php?erro", true, 303);
+            exit;
+        }
+    }
 ?>
